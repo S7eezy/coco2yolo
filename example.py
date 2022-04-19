357ccdbec48dbccd76d6cdc4fbc2c14c21ebdf39
S7eezy <cvrc.valentin@gmail.com>
2022-04-19 17:18:05 +0200

    Adapt to new arguments

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
 from coco2yolo import coco2yolo
 
 if __name__ == "__main__":
-    coco2yolo(COCO_CATEGORIES_QUERY=['car', 'person', 'bicycle'],
+    coco2yolo(CUSTOM_DATASET=True,
+              COCO_CATEGORIES_QUERY=['face mask', 'car', 'person', 'motorcycle'],
               COCO_ANNOTATIONS="instances_train2017.json",
               NUM_SAMPLES=1500,
               OUTPUT_DIRECTORY="./output",
